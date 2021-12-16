c4be4be956d4d62afdd23437ffca0272ebed6875
NguyenDuyCong <duycong.cn@gmail.com>
2021-12-16 22:12:38 +0700

    11/12

--- myApp.js
+++ myApp.js
@@ -111,8 +111,11 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
+  // done(null /*, data*/);
 };
 
 const queryChain = (done) => {
